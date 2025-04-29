33d0b5504595d11155b0f849fae28e867e2c67b1
sephrot <aliash12@hotmail.com>
2025-04-29 20:11:52 +0200

    merged server and client into one. Added arguments. Yin and Yang.

--- application.py
+++ application.py
@@ -6,26 +6,38 @@ import time
 import sys, argparse
 
 
-ack = 1
-flags = 0
-window = 5
-nextSeqSum = 1
+clientAck = 1
+clientFlags = 0
+
+serverAAck = 0
+
+nextSeqNum = 1
 
 headerFormat = '!HHHH'
 
 
-#parser = argparse.ArgumentParser(description="Optional arguments: checking IPv4 address", epilog="end of help") #sets up an arguments parser that can handle command-line inputs via variable "parser"
-#parser.add_argument('-i' , '--ip', type=str, required=True) #uses parser variable to add three arguments. Gives the argument type and if the required argument is true
-#parser.add_argument('-p' , '--port', type=int, required=True)
-#parser.add_argument('-f' , '--file', type=str, required=True)
+parser = argparse.ArgumentParser(description="Optional arguments: checking IPv4 address", epilog="end of help") #sets up an arguments parser that can handle command-line inputs via variable "parser"
+parser.add_argument('-i' , '--ip', type=str, required=True) #uses parser variable to add three arguments. Gives the argument type and if the required argument is true
+parser.add_argument('-p' , '--port', type=int, required=True)
+parser.add_argument('-f' , '--file', type=str, required=False)
+parser.add_argument('-w' , '--window', type=str, required=False)
 
-#args = parser.parse_args() #parses the command-line arguments created
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-s', '--server', action='store_true', help='Run as server')
+group.add_argument('-c', '--client', action='store_true', help='Run as client')
+
+
+args = parser.parse_args() #parses the command-line arguments created
+
 clientSocket = socket.socket(AF_INET, SOCK_DGRAM)
+serverSocket = socket.socket(AF_INET, SOCK_DGRAM)
 
-serverName = "127.0.0.1"
-serverPort = 12000
-#filepath = args.file
-
+serverName = args.ip
+serverPort = args.port
+filepath = args.file
+runServer = args.server
+runClient = args.client
+windowSize = int(args.window)
 
 def now():
 	return time.ctime(time.time())
@@ -57,21 +69,28 @@ def sendSyn(clientSocket, serverName, serverPort):
     data = createPacket(seq,ack,flags,win,None)
     clientSocket.sendto(data, (serverName, serverPort))
 
-def sendAck(clientSocket, serverAddress):
-    seq = 1
-    ack = 1
+def sendAck(clientSocket, serverAddress,seq,ack):
     flags = 0b0010
     win = 5
     data = createPacket(seq,ack,flags,win,None)
     clientSocket.sendto(data, (serverAddress))
 
+def sendSynAck(serverSocket, clientAddress):
+    seq = 0
+    ack = 1
+    flags = 0b0110
+    win = 5
+    data = createPacket(seq,ack,flags,win,None)
+    serverSocket.sendto(data, (clientAddress))
+
+
 def finish(clientSocket, serverName, serverPort):
+    global nextSeqNum, clientAck
     flags = 0b1000
-    data = createPacket(nextSeqNum, ack, flags, window, None)
+    data = createPacket(nextSeqNum, clientAck, flags, windowSize, None)
     clientSocket.sendto(data, (serverName,serverPort))
 
 def clientHandshake():
-    global seqNum
     while True:
         sendSyn(clientSocket, serverName, serverPort)
         print("SYN packet is sent")
@@ -87,8 +106,7 @@ def clientHandshake():
             #Here we're going to check if the flag is a SYN
             if serverFlags == 6: #STAGE 3
                 print("SYN-ACK packet is recieved") #if the IF hits, it means the server got our packet and we got a SYN-ACK back.
-                sendAck(clientSocket, serverAddress)
-                seqNum+=1
+                sendAck(clientSocket, serverAddress,1,clientAck)
                 print("ACK packet is sent")
                 print("Connection Established\n")
                 break  
@@ -101,9 +119,9 @@ def clientSending():
     print("Connection Establishment Phase: \n")
     global nextSeqNum
     base = 0
-    flags = 0
+    clientFlags = 0
     nextSeqNum = 1
-    ack = 1
+    clientAck = 1
     windowlist = []
     datalist = {}
     clientSocket.settimeout(0.4)
@@ -112,13 +130,13 @@ def clientSending():
     except Exception as e:
         print(e, " Some error happened during the handshake")
       
-    with open("test.jpg", "rb") as f:
+    with open(filepath, "rb") as f:
         while True:
-            while nextSeqNum <= base + window:    
+            while nextSeqNum <= base + windowSize:    
                 packet = f.read(992)
                 if not packet:
                     break
-                data = createPacket(nextSeqNum, ack, flags, window, packet)
+                data = createPacket(nextSeqNum, clientAck, clientFlags, windowSize, packet)
                 windowlist.append(nextSeqNum)
                 datalist[nextSeqNum] = data
                 clientSocket.sendto(data, (serverName, serverPort))
@@ -158,13 +176,92 @@ def clientSending():
                 for seq in windowlist:
                     clientSocket.sendto(datalist[seq],(serverName, serverPort))
 
-        
-def main():
     
-    clientSending()
-                
+def serverHandshake():
+    serverSocket.bind((serverName, serverPort))
+    global seq, serverAAck
+    while True:
+        try:
+            message, clientAddress = serverSocket.recvfrom(1000)
+            clientHeader = parseHeader(message[:8])
+            
+            clientSeqNum = clientHeader[0]
+            cAckNum = clientHeader[1] #the clients expected ack num from the server
+            cFlags = clientHeader[2]
+
+            data = message[8:]
+
+            if cFlags == 4:
+                serverAAck += 1
+                print("SYN packet is recieved")
+                sendSynAck(serverSocket, clientAddress)
+                print("Amount of times client has sent to me: ", clientSeqNum)    
+                print("SYN-ACK packet is sent")
+                seq+=1
+
+            elif cFlags == 2:
+                print("Server: Connection established")
+                print("Ready to recieve data")
+                break
+        except Exception as e:
+            print("Something went wrong.", e)
+
+def serverSide():
+    global serverAAck, seq
+    print(serverAAck)
+    serverHandshake()
+    print(serverAAck)
+    flags = 0
+    received_packets = {}
+    while True:
+        
+        try:
+            message, clientAddress = serverSocket.recvfrom(1000)
+            clientHeader = parseHeader(message[:8])
+            
+            clientSeqNum = clientHeader[0]
+            clientFlag = clientHeader[2]
+            data = message[8:]     
+            
+            if serverAAck == clientSeqNum:
+                print("Expected: ", serverAAck, " Actual: ", clientSeqNum, "\n")
+                received_packets[clientSeqNum] = data
+                sendAck(serverSocket, clientAddress, 1, serverAAck)
+                serverAAck += 1
+            else:
+                sendAck(serverSocket, clientAddress, 1, serverAAck)
+                if clientSeqNum not in received_packets:
+                    received_packets[clientSeqNum] = data
+                    
+
+            if clientFlag == 0b1000:
+                print("FIN packet is recieved")
+                flags = 0b1010
+                data = createPacket(1, serverAAck, flags, 5, None)
+                serverSocket.sendto(data, clientAddress)
+                print("Fin ACK packet is sent.")
+                print("Connection Closes")
+
+
+                break
+        except ConnectionResetError:
+            print("No more data left!")
+            break
+          
+    
+    with open("1.jpg", "wb") as f:
+        for i in sorted(received_packets.keys()):
+            f.write(received_packets[i])
+
+
+def main():
+    if runClient:
+        print("Starting server...")      
+        clientSending()
+    if runServer:
+        print("Staring server...")
+        serverSide()
 
 if __name__ == "__main__":
     main()
-
 
